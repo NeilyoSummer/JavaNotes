971b9063ed3f6ac7fc3ebe4b52f9a510bc83c3a4
neilyo <neilyosummer@163.com>
2018-11-17 22:59:54 +0800

    两个电话类对象的使用和示意图

---
+++ day06-code/src/cn/itcast/day06/demo02/Demo02PhoneTwo.java
@@ -0,0 +1,48 @@
+package cn.itcast.day06.demo02;
+
+/*
+成员方法保存的其实是一个地址值
+ */
+public class Demo02PhoneTwo {
+    public static void main(String[] args) {
+        Phone one = new Phone();
+        System.out.println(one.brand); // null
+        System.out.println(one.price); // 0.0
+        System.out.println(one.color); // null
+        printSplitLine();
+
+        one.brand = "Apple";
+        one.price = 8388.0;
+        one.color = "黑色";
+        System.out.println(one.brand); // Apple
+        System.out.println(one.price); // 8388.0
+        System.out.println(one.color); // 黑色
+        printSplitLine();
+
+        one.call("Jobs");
+        one.sendMessage();
+
+        printSplitLine();
+
+        Phone two = new Phone();
+        System.out.println(two.brand); // null
+        System.out.println(two.price); // 0.0
+        System.out.println(two.color); // null
+        printSplitLine();
+
+        two.brand = "SAMSUNG";
+        two.price = 5999.0;
+        two.color = "蓝色";
+        System.out.println(two.brand); // SAMSUNG
+        System.out.println(two.price); // 5999.0
+        System.out.println(two.color); // 蓝色
+        printSplitLine();
+
+        two.call("欧巴");
+        two.sendMessage();
+    }
+
+    public static void printSplitLine() {
+        System.out.println("=======================");
+    }
+}
