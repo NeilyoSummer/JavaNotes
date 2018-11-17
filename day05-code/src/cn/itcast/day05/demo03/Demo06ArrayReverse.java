26cfb843ce3293458a1b7feb0c28c9b7cda62020
alexskywin <alexskywin@163.com>
2018-11-17 08:46:07 +0800

    数组的反转

--- day05-code/src/cn/itcast/day05/demo03/Demo06ArrayReverse.java
+++ day05-code/src/cn/itcast/day05/demo03/Demo06ArrayReverse.java
@@ -14,9 +14,36 @@ package cn.itcast.day05.demo03;
     int temp = a;
     a = b;
     b = temp;
+4. 什么时候停止交换
+    min < max停止交换
  */
 public class Demo06ArrayReverse {
     public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 50};
 
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        System.out.println("=====================");
+
+        /*
+        初始化语句：
+            int min = 0, max = array.length - 1;
+        条件判断：
+            min < max
+        步进表达式：
+            min++, max--
+        循环体：
+            利用三个变量倒手
+         */
+        for(int min=0,max=array.length - 1; min < max; min++, max--) {
+            int temp = array[min];
+            array[min] = array[max];
+            array[max] = temp;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
 }
