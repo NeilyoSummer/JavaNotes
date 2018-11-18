9849661df1fc55bc41031f5663a2964d66b2a94e
alexskywin <alexskywin@163.com>
2018-11-19 00:13:14 +0800

    字符串比较

---
+++ day08-code/src/cn/itcast/day08/demo02/Demo01StringEquals.java
@@ -0,0 +1,10 @@
+package cn.itcast.day08.demo02;
+
+/*
+== 是进行对象的地址值比较，如果确实需要字符串的内容比较，可以使用两个方法：
+public boolean equals(Object obj)：参数可以是任何对象，只有参数是一个字符串并且内容相同才会返回true，否则返回false
+备注：任何对象都能用Object进行接收。
+ */
+public class Demo01StringEquals {
+
+}
