db6da3494598f467d8239f48240602a2ed00891a
alexskywin <alexskywin@163.com>
2018-11-23 21:54:43 +0800

    继承

---
+++ day09-code/src/cn/itcast/day09/demo06/Phone.java
@@ -0,0 +1,16 @@
+package cn.itcast.day09.demo06;
+
+// 老款手机
+public class Phone {
+    public void call() {
+        System.out.println("打电话");
+    }
+
+    public void send() {
+        System.out.println("发信息");
+    }
+
+    public void show() {
+        System.out.println("显示号码");
+    }
+}
