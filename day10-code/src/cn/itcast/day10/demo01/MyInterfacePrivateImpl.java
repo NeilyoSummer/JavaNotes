d55b7731058ff73c043cfbdea2a2b476524c613f
alexskywin <alexskywin@163.com>
2018-11-24 11:42:00 +0800

    Java 9接口私有方法

---
+++ day10-code/src/cn/itcast/day10/demo01/MyInterfacePrivateImpl.java
@@ -0,0 +1,7 @@
+package cn.itcast.day10.demo01;
+
+public class MyInterfacePrivateImpl implements MyInterfaePrivateA {
+    public void methodAnother() {
+        // methodCommon();
+    }
+}
---
+++ day10-code/src/cn/itcast/day10/demo01/MyInterfaePrivateB.java
@@ -0,0 +1,19 @@
+package cn.itcast.day10.demo01;
+
+public interface MyInterfaePrivateB {
+    public static void methodDefault1() {
+        System.out.println("静态方法1");
+        methodStaticCommon();
+    }
+
+    public static void methodDefault2() {
+        System.out.println("静态方法2");
+        methodStaticCommon();
+    }
+
+    private static void methodStaticCommon() {
+        System.out.println("AAA");
+        System.out.println("BBB");
+        System.out.println("CCC");
+    }
+}
