5639ff7be7f428f4205ed7f1c2eb12e3647481f0
alexskywin <alexskywin@163.com>
2018-11-24 14:43:59 +0800

    用instanceof关键字进行类型判断

--- day10-code/src/cn/itcast/day10/demo06/Cat.java
+++ day10-code/src/cn/itcast/day10/demo06/Cat.java
@@ -5,4 +5,9 @@ public class Cat extends Animal {
     public void eat() {
         System.out.println("猫吃鱼");
     }
+
+    // 子类特有方法
+    public void catchMouse() {
+        System.out.println("猫抓老鼠");
+    }
 }
